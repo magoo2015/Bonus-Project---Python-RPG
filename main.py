dbc7ae7099dd41965ed0cd922bff492715d799d1
Sydney McGee <mcgee.sydney@gmail.com>
2022-05-08 00:40:19 -0500

    First commit, started character dictionary

---
+++ main.py
@@ -0,0 +1,14 @@
+characters = {
+    "Hercules" : {
+        "Attack" : 50,
+        "Health" : 100,
+        "Attack Names" : ["Zeus Punch", "Ares Kick", "Sword Slash"]
+    },
+   "Nemean Lion" : {
+       "Attack" : 20,
+       "Health" : 100,
+       "Attack Names" : ["Lion's Mame", "Poison Bite", "Spinning Tail Whip"]
+   }
+}
+
+print(characters)
